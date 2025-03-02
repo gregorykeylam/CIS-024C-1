de30070394bc8ab2e926bc36ed096d041fb6b1ee
gregorykeylam <gregorykeylam@gmail.com>
2025-03-02 04:08:53 -0800

    Added basic header containing basic info about the file

--- src/HelloWorld.py
+++ src/HelloWorld.py
@@ -1,6 +1,9 @@
-"""
-    This is the hello world program in python
-"""
+# Filename: HelloWorld.py
+# Author: Gregory Lam
+# Created: March 2, 2025
+# Description: A hello world program that also prints out the python version number.
+
+
 import sys
 
 # print the version of python you are running, it should be 3.x
